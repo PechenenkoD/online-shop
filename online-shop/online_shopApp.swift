9d780eb23d0a85e951747460b9a414bdbc84e794
Dima <pechenenkod@gmail.com>
2022-09-15 15:40:25 +0300

    Add new project

---
+++ online-shop/online_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  online_shopApp.swift
+//  online-shop
+//
+//  Created by Dima Pechenenko on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct online_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
